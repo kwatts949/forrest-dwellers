e2c87ce469e673bdc50abc3484ae562c9267e128
Kayleigh Watts <kwatts949@gmail.com>
2022-11-07 16:00:00 +0000

    Add database tables G & K

---
+++ seeds/users_table.sql
@@ -0,0 +1,12 @@
+CREATE TABLE users ( id SERIAL PRIMARY KEY, username text,
+email text,
+password varchar
+);
+
+CREATE TABLE spaces ( id SERIAL PRIMARY KEY, space_name text,
+description text,
+price float,
+booked bool,
+user_id int,
+constraint fk_user foreign key(user_id) references users(id) on delete cascade
+);
