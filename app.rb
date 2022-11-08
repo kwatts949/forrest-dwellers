39bbd4b5e468b8d99dfc5470132a09b84eb5cf8f
Mykhailo Skliarenko <painliader@gmail.com>
2022-11-08 13:53:20 +0000

    Spaces list page connected to database (without links to each space)

--- app.rb
+++ app.rb
@@ -1,9 +1,12 @@
 require 'sinatra/base'
 require 'sinatra/reloader'
+require_relative './lib/database_connection'
+require_relative './lib/space_repository'
 
 class Application < Sinatra::Base
   configure :development do
     register Sinatra::Reloader
+    DatabaseConnection.connect('makersbnb_test')
   end
 
   get '/' do
@@ -11,6 +14,8 @@ class Application < Sinatra::Base
   end
 
   get '/spaces' do
+    repo = SpaceRepository.new
+    @spaces = repo.all
     return erb(:spaces)
   end
 end
