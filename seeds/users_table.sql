921ca4100461531c5f459c917473c26a6d323180
Kayleigh Watts <kwatts949@gmail.com>
2022-11-08 11:35:16 +0000

    Add space classes and tests

--- seeds/users_table.sql
+++ seeds/users_table.sql
@@ -6,7 +6,6 @@ password varchar
 CREATE TABLE spaces ( id SERIAL PRIMARY KEY, space_name text,
 description text,
 price float,
-booked bool,
 user_id int,
 constraint fk_user foreign key(user_id) references users(id) on delete cascade
 );
