15a5c0a25f2c0ef9e59d42d7cf3cdffbed9fcbcb
Kayleigh Watts <kwatts949@gmail.com>
2022-11-08 13:45:34 +0000

    Add find by id method to Space Class

--- lib/space_repository.rb
+++ lib/space_repository.rb
@@ -20,11 +20,24 @@ class SpaceRepository
   end
 
     def create(space)
-    sql = 'INSERT INTO spaces (space_name, description, price, user_id) VALUES ($1, $2, $3, $4);'
-    sql_params = [space.space_name, space.description, space.price, space.user_id]
+      sql = 'INSERT INTO spaces (space_name, description, price, user_id) VALUES ($1, $2, $3, $4);'
+      sql_params = [space.space_name, space.description, space.price, space.user_id]
 
-    result_set = DatabaseConnection.exec_params(sql, sql_params)
+      result_set = DatabaseConnection.exec_params(sql, sql_params)
 
-    return nil
-  end
+      return nil
+    end
+
+    def find(id)
+      sql = 'SELECT id, space_name, description, price FROM spaces WHERE id = $1;'
+      result_set = DatabaseConnection.exec_params(sql, [id])
+
+      space = Space.new
+      space.id = result_set[0]['id'].to_i
+      space.space_name = result_set[0]['space_name']
+      space.description = result_set[0]['description']
+      space.price = result_set[0]['price'].to_i
+
+      return space
+    end
 end
